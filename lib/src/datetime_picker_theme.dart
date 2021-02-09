f0cc3e74d1bed7ca922dde6eed1b801e48712edc
AnhDV580 <69618097+AnhDV580@users.noreply.github.com>
2021-02-09 09:44:14 +0700

    Update datetime_picker_theme.dart

--- lib/src/datetime_picker_theme.dart
+++ lib/src/datetime_picker_theme.dart
@@ -17,7 +17,8 @@ class DatePickerTheme with DiagnosticableTreeMixin {
   const DatePickerTheme({
     this.cancelStyle = const TextStyle(color: Colors.black54, fontSize: 16),
     this.doneStyle = const TextStyle(color: Colors.blue, fontSize: 16),
-    this.itemStyle = const TextStyle(color: Color(0xFF000046), fontSize: 18),
+    //this.itemStyle = const TextStyle(color: Color(0xFF000046), fontSize: 18),
+    this.itemStyle = const TextStyle(fontSize: 16),
     this.backgroundColor = Colors.white,
     this.headerColor,
     this.containerHeight = 210.0,
